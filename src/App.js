9fb3032ee38541c0ee9fc43f3f2b88bb4dc8ed30
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-07-26 11:43:35 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 
 import './App.css';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import NavBar from './components/NavBar.js'
+
 
 function App() {
 
   return (
     <>
-      <p>Jess</p>
       
-      
+      <Router>
+      <NavBar/>
+        <Routes>
+          <Route exact path="/"/>
+                <Route  path="/home" element={<Home />}/>
+
+        
+      <Route path="/about" element={<About/>}/>
+        {/* <About /> */}
+        {/* </Route> */}
+    </Routes>
+    </Router>
+    
+
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,45 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
+// function Home() {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// }
 
-function About() {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
-}
+// function About() {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// }
 
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+  // <Router>
+  //   <Routes>
+  //     <Route path="/">
+  //       <Home />
+  //     </Route>
+  //     <Route path="/about">
+  //       <About />
+  //     </Route>
+  //   </Routes>
+  // </Router>,
+//   document.getElementById("root")
+// );
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <Router> */}
     <App />
+    {/* </Router> */}
   </React.StrictMode>
 );
 
